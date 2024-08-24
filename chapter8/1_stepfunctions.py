d8774add44c5f0a1931a8a74257f9a3fdf55c8ac
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2024-08-24 21:45:26 +0900

    Add files of chapter8

---
+++ chapter8/1_stepfunctions.py
@@ -0,0 +1,66 @@
+# Pyhton外部モジュールのインポート
+import base64
+import json
+from io import BytesIO
+
+import boto3
+import streamlit as st
+from PIL import Image
+
+my_session = boto3.Session(profile_name="default", region_name="us-east-1")
+
+# タイトル
+st.title("自己紹介アプリ")
+
+
+# バケット情報を分割
+def split_bucket_info(arn: str):
+    from urllib.parse import urlparse
+
+    result = urlparse(arn)
+    bucket_name = result.netloc
+    key = result.path[1:]
+    return bucket_name, key
+
+
+# 画像ファイルを取得
+def get_object(arn: str):
+    bucket_name, key = split_bucket_info(arn)
+
+    s3 = my_session.client("s3")
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+
+    return json.loads(response["Body"].read().decode("utf-8"))
+
+
+# 入力フォームを生成
+with st.form("form"):
+    state_machine_arn = st.text_input("stateMachineArn")
+    submit = st.form_submit_button("実行")
+
+# 実行ボタンを押した際の処理
+if submit:
+    # Step Functionsクライアントを生成
+    sfn_client = my_session.client("stepfunctions")
+
+    # ステートマシンを実行
+    response = sfn_client.start_sync_execution(stateMachineArn=state_machine_arn)
+
+    # 実行結果を画面に表示
+    st.json(response, expanded=False)
+
+    output = json.loads(response["output"])
+
+    for o in output:
+        # 自己紹介を画面に表示
+        if "Markdown" in o:
+            completion = o["Markdown"]["Body"]["content"][0]["text"]
+            st.markdown("# " + completion)
+
+        # 画像を画面に表示
+        if "Image" in o:
+            s3_arn = o["Image"]["Body"]
+            body = get_object(s3_arn)
+            image_base64 = body["artifacts"][0]["base64"]
+
+            st.image(Image.open(BytesIO(base64.b64decode(image_base64))))
---
+++ chapter8/README.md
@@ -0,0 +1,16 @@
+# 第８章 生成 AI アプリをローコードで開発しよう
+
+- Step Function と他の AWS サービスとの統合の種類
+  - 最適化された統合
+    - Bedrock
+      - InvokeModel API
+      - CreateModelCustomizationJob API
+  - AWS SDK の統合
+    - API Parameter を Json で指定
+- Bedrock と Step Functions を使った生成 AI アプリ開発
+- ハンズオンの概要
+  1. Bedrock に関する投稿を取得する
+  2. 取得した各投稿の要点をまとめる
+  3. 自己紹介とキャッチコピーを作成する
+  4. 生成したものを Markdown 形式に変換する
+  5. アイキャッチ画像を生成する
